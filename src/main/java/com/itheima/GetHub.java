9c489672cced041dd58b4be813fdc194ca851562
xiaofeifei <497042325@qq.com>
2019-08-16 17:28:28 +0800

    增加了一条代码

--- src/main/java/com/itheima/GetHub.java
+++ src/main/java/com/itheima/GetHub.java
@@ -6,4 +6,7 @@ public class GetHub {
     private String name1;
     private String name2;
     private String name3;
+    private String name4;
+    private String name6;
+    private String name7;
 }
